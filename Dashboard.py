68f79c5fda1fc92d0de6d33a8dc9678a5a705d69
HibaBoulhanna <80213135+HibaBoulhanna@users.noreply.github.com>
2022-06-14 18:04:05 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -44,16 +44,14 @@ st.markdown("Sous la direction de Mr.HOUMMANI Ayoub & Mr.BENABADJI Oualid")
 st.markdown('__________________________________________________________')
 
 
-<<<<<<< HEAD
+
 dropdown = st.sidebar.selectbox("Choisir une action", pd.concat([pd.Series(["MASI"]), py.get_stocks(country='morocco').name]))
 indicateur = st.sidebar.selectbox("Choisir un indicateur", ['MACD','RSI', 'BB'])
-=======
-dropdown = st.sidebar.selectbox("Choisir une action", py.get_stocks(country='morocco').name)
-indicateur = st.sidebar.selectbox("Choisir un indicateur", ['MACD','RSI'])
 
-ma = st.sidebar.selectbox("Période de calcul de la moyenne mobile (en jours)", [15,30,45,60])
 
->>>>>>> 2664a1ebed214a99a0cc24ea8e3051467eeae1aa
+
+
+
 
 
 start = st.sidebar.date_input('Debut', value =pd.to_datetime('01-01-2020'))
