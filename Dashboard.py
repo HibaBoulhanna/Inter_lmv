c0933ed1ea234947ecf1f76987bb6908a77a7bb5
Yassine Rhzif <40427561+RHZIF@users.noreply.github.com>
2022-09-27 01:11:33 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -11,6 +11,41 @@ from Indicators import  macd, sign_macd1, smm, stochastic, rate_of_change, momen
 # st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
+def get_logo(ticker):
+    headers = {
+            'authority': 'www.tradingview.com',
+            'cache-control': 'max-age=0',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'accept-language': 'en-US,en;q=0.9,fr;q=0.8',
+        }
+
+    try : 
+        url = f'https://www.tradingview.com/symbols/NYSE-{ticker}/'
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, "html.parser")
+        img = soup.find("div", {"class": "js-sticky-symbol-header-container tv-sticky-symbol-header"})
+        img_url = img["data-logo-urls"]
+        return img_url
+    except :
+        try: 
+            url = f'https://www.tradingview.com/symbols/NASDAQ-{ticker}/'
+            r = requests.get(url, headers=headers, cookies=cookies)
+            soup = BeautifulSoup(r.text, "html.parser")
+            img = soup.find("div", {"class": "js-sticky-symbol-header-container tv-sticky-symbol-header"})
+            img_url = img["data-logo-urls"]
+            return img_url
+        except :
+            return ''
+       
 apptitle = 'Projet hiba'
 
 st.set_page_config(page_title=apptitle, page_icon=":chart_with_upwards_trend:")
@@ -42,6 +77,15 @@ start = start.strftime('%Y-%m-%d')
 end = end.strftime('%Y-%m-%d')
 
 df = yf.download(dropdown, start=start, end=end)
+
+
+url = get_logo(dropdown)
+url = str(url)
+
+st.markdown('__________________________________________________________')
+st.markdown('<center>  '+dropdown+'                <img src="'+url+'" alt="stock logo"></center>', unsafe_allow_html=True)
+st.markdown('__________________________________________________________')
+
 st.markdown('__________________________________________________________')
 
 if indicateur == 'MACD':
